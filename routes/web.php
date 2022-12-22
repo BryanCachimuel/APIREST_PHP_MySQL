ebf9af95c5f27fd212748190dcc80906d0db12a5
BryanCachimuel <bryanloyo56@gmail.com>
2022-12-21 22:37:01 -0500

    corrigiendo scripts del proyecto

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// agregar todos los controladores 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\RolController;
-use App\Http\Controllers\UsuarioController;
-use App\Http\Controllers\BlogController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,16 +15,4 @@ use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::group(['middleware' => ['auth']], function(){
-    Route::resource('roles', RolController::class);
-    Route::resource('usuarios', UsuarioController::class);
-    Route::resource('blogs', BlogController::class);
 });
