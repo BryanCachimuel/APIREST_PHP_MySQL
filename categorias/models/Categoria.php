e0806d5a7bd6d3157ae35b2233deae2421282202
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-08 22:19:23 -0500

    Creando la función get_categorias_por_id para obtener un registro de una categoria mediante su identificador único

--- categorias/models/Categoria.php
+++ categorias/models/Categoria.php
@@ -8,7 +8,18 @@
              $sql = "SELECT * FROM tm_categoria WHERE cat_estado=1";
              $sql = $conectar->prepare($sql);
              $sql->execute();
-             $resultado = $sql->fetchAll();
+             $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+             return $resultado;
+        }
+
+        public function get_categoria_por_id($cat_id) {
+             $conectar = parent::Conexion();
+             parent::set_names();
+             $sql = "SELECT * FROM tm_categoria WHERE cat_estado=1 AND cat_id = ?";
+             $sql = $conectar->prepare($sql);
+             $sql->bindValue(1, $cat_id);
+             $sql->execute();
+             $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
              return $resultado;
         }
 
