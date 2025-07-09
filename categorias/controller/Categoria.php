914af300f7898996eb0b60e41fb03ceea3685769
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-08 22:21:07 -0500

    Creando un case para obtener una categoria por su id mediante la utilización de un parámetro que se obtendra en el body

--- categorias/controller/Categoria.php
+++ categorias/controller/Categoria.php
@@ -1,14 +1,23 @@
 <?php
 
+    header('Content-Type: application/json');
+
     require_once("../config/conexion.php");
     require_once("../models/Categoria.php");
 
     // instancia de la clase categoria
     $categoria = new Categoria();
 
+    $body = json_decode(file_get_contents("php://input"), true);
+
     switch ($_GET["opcion"]) {
         case "GetAll":
             $datos = $categoria->get_categoria();
+            echo json_encode($datos);
+            break;
+        
+        case "GetId":
+            $datos = $categoria->get_categoria_por_id($body["cat_id"]);
             echo json_encode($datos);
             break;
         
