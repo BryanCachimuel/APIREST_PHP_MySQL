ab03832afe5dbe572b2485e73c5233d44cefa13f
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-08 22:37:23 -0500

    Creando la función insert_categoria para poder registrar los datos de una nueva categoría

--- categorias/models/Categoria.php
+++ categorias/models/Categoria.php
@@ -23,4 +23,16 @@
              return $resultado;
         }
 
+        public function insert_categoria($cat_nombre, $cat_observacion) {
+             $conectar = parent::Conexion();
+             parent::set_names();
+             $sql = "INSERT INTO tm_categoria(cat_id,cat_nombre,cat_observacion,cat_estado) VALUES(NULL,?,?,'1')";
+             $sql = $conectar->prepare($sql);
+             $sql->bindValue(1, $cat_nombre);
+             $sql->bindValue(2, $cat_observacion);
+             $sql->execute();
+             $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+             return $resultado;
+        }
+
     }
