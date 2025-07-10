2e1765bec7a6f3e772646463ad7cc65e6fe9317d
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-09 22:05:17 -0500

    Creando una clase Product donde se va a tener todos los métodos CRUD, para este caso se creo el método create para registrar nuevos productos

---
+++ productos/src/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+    class Product {
+
+        private $conn;
+        private $table_name = "tm_productos";
+
+        public $id;
+        public $nombre;
+        public $descripcion;
+        public $precio;
+        public $creado_en;
+
+        // se inicializa la petición de conexión hacia la base de datos
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        public function create() {
+            $query = "INSERT INTO " . $this->table_name . "SET nombre=:nombre, descripcion=:descripcion, precio=:precio, creado_en=NOW()";
+            $stmt = $this->conn->prepare($query);
+
+            // se limpian los atributos antes de ser ingresados
+            $this->nombre = htmlspecialchars(strip_tags($this->nombre));
+            $this->descripcion = htmlspecialchars(strip_tags($this->descripcion));
+            $this->precio = htmlspecialchars(strip_tags($this->precio));
+
+            // Parámetros que se quieren enviar para llenarlos de información
+            $stmt->bindParam(":nombre", $this->nombre);
+            $stmt->bindParam(":descripcion", $this->descripcion);
+            $stmt->bindParam(":precio", $this->precio);
+
+            // si todo esta correcto
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
+
+    }
