638f1229dfdb8207b4c4ad12ebaadfd6e574d16c
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-09 21:47:44 -0500

    Creando una clase Database donde se realiza la configuración hacia la base de datos

---
+++ productos/config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+    class Database {
+
+        private $host = "localhost";
+        private $db_name = "api-rest-categorias";
+        private $username = "root";
+        private $password = "";
+        
+        public $conn;
+
+        public function getConnection(){
+            // se asegura que no exista ninguna conexión
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO("mysql:host" . $this->host . "; dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            } catch (Exception $e) {
+                echo "Error en la conexión: " . $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+
+    }
