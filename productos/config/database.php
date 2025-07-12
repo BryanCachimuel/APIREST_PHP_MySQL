7a9e3812c8fb2f470b51be50ead3c30501e188cf
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-11 21:43:56 -0500

    Corrigiendo un error en la conexión hacia la base de datos

--- productos/config/database.php
+++ productos/config/database.php
@@ -14,7 +14,7 @@
             $this->conn = null;
 
             try {
-                $this->conn = new PDO("mysql:host" . $this->host . "; dbname=" . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
             } catch (Exception $e) {
                 echo "Error en la conexión: " . $e->getMessage();
