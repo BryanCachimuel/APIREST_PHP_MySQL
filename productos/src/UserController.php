1806309d1edd7e899661f1456ee98339d69d77a1
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-11 22:36:15 -0500

    Creando el método register el cual consume el método register del modelo User

--- productos/src/UserController.php
+++ productos/src/UserController.php
@@ -15,4 +15,25 @@
             $this->user = new User($this->db);
         }
 
+        public function register() {
+            $data = json_decode(file_get_contents("php://input"));
+
+            if(!empty($data->nombre) && !empty($data->correo) && !empty($data->contrasenia)) {
+                $this->user->nombre = $data->nombre;
+                $this->user->correo = $data->correo;
+                $this->user->contrasenia = $data->contrasenia;
+
+                if($this->user->register()) {
+                    http_response_code(201);
+                    echo json_encode(["message" => "Usuario Creado Exitosamente"]);
+                } else {
+                    http_response_code(503);
+                    echo json_encode(["message" => "Ha ocurrido un error en la creación del usuario"]);
+                }
+            } else {
+                http_response_code(400);
+                echo json_encode(["message" => "Datos de Usuario Incompletos"]);
+            }
+        }
+
     }
