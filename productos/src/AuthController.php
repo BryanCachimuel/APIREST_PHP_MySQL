406e647318090d27c8db1279c6d4b8523dd0cec0
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-14 21:16:20 -0500

    Se crea el método login en el cual se validan los campos correo y contraseña para poder realizar un correcto inicio de sesión de los usuarios

--- productos/src/AuthController.php
+++ productos/src/AuthController.php
@@ -18,5 +18,36 @@
             $this->user = new User($this->db);
         }
 
-        
+       public function login() {
+         $data = json_decode(file_get_contents("php://input"));
+
+         if(!empty($data->correo) && !empty($data->contrasenia)) {
+            $this->user->correo = $data->correo;
+            $user = $this->user->findByEmail();
+
+            if ($user && password_verify($data->contrasenia, $user["contrasenia"])) {
+                $token = [
+                    "iss" => JwtConfig::getIssuer(),
+                    "aud" => JwtConfig::getAudience(),
+                    "iat" => JwtConfig::getIssueAt(),
+                    "exp" => JwtConfig::getExpirationTime(),
+                    "data" => [
+                        "id" => $user["id"],
+                        "correo" => $user["correo"]
+                    ]
+                ];
+               
+                $jwt = JWT::encode($token, JwtConfig::getKey(), 'HS256');
+                http_response_code(200);
+                echo json_encode(["message" => "Inicio de sesión exitoso", "token" => $jwt]);
+            } else {
+                http_response_code(401);
+                echo json_encode(["message" => "Correo o Contraseña Incorrectos"]);
+            }
+            
+         }else {
+            http_response_code(400);
+            echo json_encode(["message" => "Datos Incompletos"]);
+         }
+       } 
     }
