2c6ef3ee816ae9bb1f4159fde82f281bddee0020
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-14 21:55:52 -0500

    Realizando una validación de rutas mediante los tokens para poder dar seguridad a la aplicación y sus respectivas rutas

--- productos/public/index.php
+++ productos/public/index.php
@@ -1,20 +1,57 @@
 <?php
 
 require_once "../src/ProductController.php";
+require_once "../src/AuthController.php";
+require_once "../src/UserController.php";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 $method = $_SERVER["REQUEST_METHOD"];
+$headers = getallheaders();
+$authHeader = isset($headers["Authorization"]) ? $headers["Authorization"] : "";
+
+if(preg_match("/Bearer\s(\S+)/", $authHeader, $matches)) {
+    $token = $matches[1];
+
+    try {
+       AuthController::validateToken($token); 
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode(["message" => "Acceso no autorizado", "error" => $e->getMessage()]);
+        exit();
+    }
+} else {
+    http_response_code(401);
+    echo json_encode(["message" => "Token no autorizado"]);
+    exit();
+}
+
+$page = isset($_GET["user"]) ? $_GET["user"] : "/";
+
 $productController = new ProductController();
+$userController = new UserController();
 
 switch ($method) {
     case 'POST':
-        $productController->create();
+        if ($page == "register") {
+            $userController->register();
+        } elseif($page == "create-product") {
+            $productController->create();
+        }
+        else {
+            http_response_code(404);
+            echo json_encode(["message" => "Método no permitido"]);
+        }
         break;
 
     case 'GET':
-        $productController->read();
+        if ($page == "/") {
+            $productController->read();
+        } else {
+           http_response_code(404);
+           echo json_encode(["message" => "Método no permitido"]);
+        }
         break;
 
     case 'PUT':
