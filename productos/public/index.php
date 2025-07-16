8f76ba689926798db5c4e72c219d72601dbaf8e8
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-15 22:09:27 -0500

    Agregando una validación para las rutas POST y GET de productos

--- productos/public/index.php
+++ productos/public/index.php
@@ -10,24 +10,27 @@ header("Content-Type: application/json; charset=UTF-8");
 $method = $_SERVER["REQUEST_METHOD"];
 $headers = getallheaders();
 $authHeader = isset($headers["Authorization"]) ? $headers["Authorization"] : "";
-
-if(preg_match("/Bearer\s(\S+)/", $authHeader, $matches)) {
-    $token = $matches[1];
-
-    try {
-       AuthController::validateToken($token); 
-    } catch (Exception $e) {
-        http_response_code(401);
-        echo json_encode(["message" => "Acceso no autorizado", "error" => $e->getMessage()]);
-        exit();
-    }
-} else {
-    http_response_code(401);
-    echo json_encode(["message" => "Token no autorizado"]);
-    exit();
-}
+$publicRoutes = ["register"];
 
 $page = isset($_GET["user"]) ? $_GET["user"] : "/";
+
+if (!in_array($page, $publicRoutes)) {
+    if (preg_match("/Bearer\s(\S+)/", $authHeader, $matches)) {
+        $token = $matches[1];
+
+        try {
+            AuthController::validateToken($token);
+        } catch (Exception $e) {
+            http_response_code(401);
+            echo json_encode(["message" => "Acceso no autorizado", "error" => $e->getMessage()]);
+            exit();
+        }
+    } else {
+        http_response_code(401);
+        echo json_encode(["message" => "Token no autorizado"]);
+        exit();
+    }
+}
 
 $productController = new ProductController();
 $userController = new UserController();
@@ -36,10 +39,9 @@ switch ($method) {
     case 'POST':
         if ($page == "register") {
             $userController->register();
-        } elseif($page == "create-product") {
+        } elseif ($page == "create-product") {
             $productController->create();
-        }
-        else {
+        } else {
             http_response_code(404);
             echo json_encode(["message" => "Método no permitido"]);
         }
@@ -49,8 +51,8 @@ switch ($method) {
         if ($page == "/") {
             $productController->read();
         } else {
-           http_response_code(404);
-           echo json_encode(["message" => "Método no permitido"]);
+            http_response_code(404);
+            echo json_encode(["message" => "Método no permitido"]);
         }
         break;
 
@@ -61,7 +63,7 @@ switch ($method) {
     case 'DELETE':
         $productController->delete();
         break;
-    
+
     default:
         // método que no está permitido en la API 
         http_response_code(405);
